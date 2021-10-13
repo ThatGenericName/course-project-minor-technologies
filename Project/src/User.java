4e58542c0f8f987a4d3c9ac722adbb4a7477e790
ThatGenericName <ThatGenericBusiness@outlook.com>
2021-10-13 18:50:31 -0400

    Final commit to Master before starting on Skeleton program.
    Let me know on discord if the class you are working on has ambiguous docstrings, or is missing docstrings for methods entirely.

--- Project/src/User.java
+++ Project/src/User.java
@@ -14,7 +14,7 @@ public class User {
      * Adds a listing to watchedListings. If listing is already in watchedListing, returns false, otherwise returns true.
      *
      * @param listing
-     * @return
+     * @return a boolean, false if listing is already in watchedListing, otherwise returns true.
      */
     public boolean addListingToWatch(Listing listing){
         throw new java.lang.UnsupportedOperationException();
