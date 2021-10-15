35ba8256dc56f7a090ae9baba28acf1f65a8c3f8
muditchandna <mudit.chandna@mail.utoronto.ca>
2021-10-15 17:25:12 -0400

    Final commit to mudit

---
+++ Project/test/DataFormatTest.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+
+public class DataFormatTest {
+    Listing l1 = Listing("Software engineer", "Toronto", 100000, JobType jobType, "College Degree",
+            "1 year experience", "Resume and Cover letter", "Engineering in Python and others");
+
+    @Test
+    public testCreateListing() {
+
+    }
+
+    @Test
+    public testCreateJSON() {
+
+    }
+}
