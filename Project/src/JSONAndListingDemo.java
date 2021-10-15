dc548285452f0782ff43714039b4e2b507630bd4
ThatGenericName <ThatGenericBusiness@outlook.com>
2021-10-15 00:57:53 -0400

    Background Operations is working.
    If you want to test this, run JSONAndListingDemo, A loop will iterate 15 times, (once every 10 seconds). During this time, use another text editor (not Intellij as it will not update files while it is running/debugging java.) to edit DemoListing2.json in DemoListings

--- Project/src/JSONAndListingDemo.java
+++ Project/src/JSONAndListingDemo.java
@@ -6,9 +6,13 @@ import java.io.IOException;
  *
  */
 public class JSONAndListingDemo {
+
+    public static CustomListing demo1 = null;
+
     public static void main(String[] args) {
         System.out.println("Go Minor Technologies!");
 
+        Main.user = new User("Test");
 
         //Creates a CustomListing through a constructor
         CustomListing demoListing = new CustomListing("DemoTitle", "DemoLocation", 36000, JobType.FULL_TIME,
@@ -19,6 +23,7 @@ public class JSONAndListingDemo {
         String demoListingJson = DataFormat.createJSON(demoListing);
 
         // writes to \DemoListing\DemoListing1.json
+        Listing demoListing2 = null;
 
         if (FileIO.WriteFile("\\DemoListings", "\\DemoListing1.json", demoListingJson)){
             System.out.println("File Written!");
@@ -33,11 +38,39 @@ public class JSONAndListingDemo {
             Listing demoListing1copy = DataFormat.createListing(FileIO.ReadFile("\\DemoListings\\DemoListing1.json"));
 
             //heres a completely new listing, read from DemoListing2.json
-            Listing demoListing2 = DataFormat.createListing(FileIO.ReadFile("\\DemoListings\\DemoListing2.json"));
+            demoListing2 = DataFormat.createListing(FileIO.ReadFile("\\DemoListings\\DemoListing2.json"));
             System.out.println(demoListing.equals(demoListing1copy));
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        Main.user.addListingToWatch(demoListing2);
+
+        BackgroundOperations.startBackgroundLoop();
+
+        int x = 0;
+
+        while(x < 15){
+            System.out.print("this is loop {");
+            System.out.print(x);
+            System.out.println("}");
+            if (!(demo1 == null)){
+                System.out.println(demo1.getDescription());
+            }
+            else{
+                System.out.println("null");
+            }
+            x++;
+
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        BackgroundOperations.endBackgroundThreads();
+
+        System.out.println("ended");
     }
 }
