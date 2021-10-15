57fbe1b0f20269c95790f94e5224b16cd1a3fb8c
muditchandna <mudit.chandna@mail.utoronto.ca>
2021-10-15 17:02:24 -0400

    Final commit to mudit

--- Project/test/UserTest.java
+++ Project/test/UserTest.java
@@ -1,4 +1,17 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.*;
+import java.util.ArrayList;
 
+class UserTest {
+    User u1 = new User("Jack");
+    Listing l1 = Listing("Software engineer", "Toronto", 100000, JobType jobType, "College Degree",
+            "1 year experience", "Resume and Cover letter", "Engineering in Python and others");
+
+    @Test
+    public testAddListingToWatch() {
+        u1.addListingToWatch(l1);
+        ArrayList <Listing> test = new ArrayList<>();
+        test.add(l1)
+
+        assertEquals(u1.watchedListings, test);
+    }
+}
