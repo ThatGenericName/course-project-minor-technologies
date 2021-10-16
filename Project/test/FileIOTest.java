1f5903be6296bba27e03e57f7d61f2af8e83381e
muditchandna <mudit.chandna@mail.utoronto.ca>
2021-10-16 17:48:43 -0400

    first commit to mudit

---
+++ Project/test/FileIOTest.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import org.junit.*;
+
+public class FileIOTest {
+    String relFilePath = "./data-files/record.txt";
+
+    @Test
+    public testReadFile() {
+
+    }
+
+    @Test
+    public testWriteFile() {
+
+    }
+
+    @Test
+    public testGetFileNamesInDir() {
+
+    }
+
+}
