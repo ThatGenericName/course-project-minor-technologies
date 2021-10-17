7e6f72d3ef314952e27d3c8d78f5136da0a614ee
ThatGenericName <ThatGenericBusiness@outlook.com>
2021-10-16 22:58:02 -0400

    Removed some test that would have required some complicated implementation

--- Project/test/DataFormatTest.java
+++ Project/test/DataFormatTest.java
@@ -10,29 +10,31 @@ public class DataFormatTest {
     JobType j1 = JobType.FULL_TIME;
     CustomListing l1 = new CustomListing("Software engineer", "Toronto", 100000, j1, "College Degree",
             "1 year experience", "Resume and Cover letter", "Engineering in Python and others", "LinkedIn");
-    JSONObject obj = new JSONObject();
+    JSONObject jsonObj = new JSONObject();
 
     @Before
     public void setUp() {
-        obj.put("UID", l1.getUID());
-        obj.put("listingType", ListingType.CUSTOM);
-        obj.put("title", l1.getTitle());
-        obj.put("location", l1.getLocation());
-        obj.put("pay", l1.getPay());
-        obj.put("jobType", l1.getJobType());
-        obj.put("qualifications", l1.getQualifications());
-        obj.put("requirements", l1.getRequirements());
-        obj.put("applicationReq", l1.getApplicationRequirements());
-        obj.put("description", l1.getDescription());
-        obj.put("saved", false);
-        obj.put("listingDate", l1.getListingDate());
-        obj.put("crossPlatformDuplicates", l1.getCPDIDs());
+        jsonObj.put("UID", l1.getUID());
+        jsonObj.put("listingType", ListingType.CUSTOM);
+        jsonObj.put("title", l1.getTitle());
+        jsonObj.put("location", l1.getLocation());
+        jsonObj.put("pay", l1.getPay());
+        jsonObj.put("jobType", l1.getJobType());
+        jsonObj.put("qualifications", l1.getQualifications());
+        jsonObj.put("requirements", l1.getRequirements());
+        jsonObj.put("applicationReq", l1.getApplicationRequirements());
+        jsonObj.put("description", l1.getDescription());
+        jsonObj.put("saved", false);
+        jsonObj.put("listingDate", l1.getListingDate());
+        jsonObj.put("crossPlatformDuplicates", l1.getCPDIDs());
+        jsonObj.put("origin", l1.getOrigin());
     }
 
     @Test
     public void testCreateListing() {
         try{
-            assertEquals(DataFormat.createListing(obj.toString()).getTitle(), l1.getTitle());
+            Listing listing = DataFormat.createListing(jsonObj.toString());
+            assertEquals(listing, l1);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Project/test/LocalCacheTest.java
+++ Project/test/LocalCacheTest.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 import org.junit.*;
@@ -6,7 +10,6 @@ import static org.junit.Assert.*;
 public class LocalCacheTest {
     JobType j1 = JobType.FULL_TIME;
     ListingType lt = ListingType.CUSTOM;
-    LocalCache lc;
     CustomListing l1 = new CustomListing("Software engineer", "Toronto", 100000, j1, "College Degree",
             "1 year experience", "Resume and Cover letter", "Engineering in Python and others", "LinkedIn");
     int u1 = l1.getUID();
@@ -16,31 +19,5 @@ public class LocalCacheTest {
     public void setUp() {
         LocalCache.listingsMap.put(l1.listingType, new ArrayList<>());
         LocalCache.listingsMap.get(l1.listingType).add(l1);
-    }
-
-    @Test
-    public void testLoadSavedListings() {
-
-    }
-
-    @Test
-    public void testSaveAllListings() {
-
-    }
-
-    @Test
-    public void testLoadListingFromUID() {
-        CustomListing l2 = new CustomListing("Data automator", "China", 99, j1, "College Degree",
-                "1 year experience", "Resume and Cover letter", "Engineering in Python and others", "LinkedIn");
-        l1.setTitle("Data automator");
-        l1.setLocation("China");
-        l1.setPay(99);
-        LocalCache.loadListingFromUID(u1);
-        for (int i = 0; i < LocalCache.listingsMap.get(lt).size(); i++) {
-            if (LocalCache.listingsMap.get(lt).get(i).getUID() == l2.getUID()){
-                u2 = i;
-            }
-        }
-        assertEquals(LocalCache.listingsMap.get(l1.listingType).get(u2), l2);
     }
 }
