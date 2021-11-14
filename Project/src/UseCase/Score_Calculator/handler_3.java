43c55594244ae6952dbb599302ab9f70b7f8e030
Domenica Vega <domenica.vega@mail.utoronto.ca>
2021-11-14 03:03:42 -0500

    score_calculate() computes score based on user's unrelated work experiences

--- Project/src/UseCase/Score_Calculator/handler_3.java
+++ Project/src/UseCase/Score_Calculator/handler_3.java
@@ -2,10 +2,12 @@ package UseCase.Score_Calculator;
 
 import Entities.Listing.JobListing;
 import Entities.User.User;
+import Entities.User.Experience;
 
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class handler_3 extends handler_ext_work{
 
@@ -17,11 +19,17 @@ public class handler_3 extends handler_ext_work{
     @Override
     protected void score_calculate() {
         double score = 0.0;
-        LocalDate start_date = (LocalDate) this.user.getData(User.UREL_WORK_EXP);
-        LocalDate end_date = (LocalDate) this.user.getData(User.UREL_WORK_EXP);
-        String title = (String) this.user.getData(User.UREL_WORK_EXP);
-        ArrayList<String> description = (ArrayList<String>) this.user.getData(User.UREL_WORK_EXP);
-        score = score_calc_time(start_date, end_date) * description.size() * 0.5;
+        ArrayList<Experience> user_experiences = (ArrayList<Experience>) this.user.getData(User.UREL_WORK_EXP);
+        for(Experience experience : user_experiences) {
+            HashMap<String, Object> experience_data = experience.serialize();
+            LocalDate start_date = (LocalDate) experience_data.get(experience.START_TIME);
+            LocalDate end_date = (LocalDate) experience_data.get(experience.END_TIME);
+            String title = (String) experience_data.get(experience.EXPERIENCE_TITLE);
+            String description = (String) experience_data.get(experience.EXPERIENCE_DESCRPTION);
+            double experience_score = score_calc_time(start_date, end_date);
+            score += experience_score * 0.5;
+        }
+
         handler_main.score += score;
     }
 
