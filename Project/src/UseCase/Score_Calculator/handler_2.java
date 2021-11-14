3a239b54c12c7f824f20b0c1f77b31fe8834bf12
Domenica Vega <domenica.vega@mail.utoronto.ca>
2021-11-14 13:39:42 -0500

    Modified score_calculate() which computes score based on user's related work experiences

--- Project/src/UseCase/Score_Calculator/handler_2.java
+++ Project/src/UseCase/Score_Calculator/handler_2.java
@@ -21,11 +21,10 @@ public class handler_2 extends handler_ext_work{
         double score = 0.0;
         ArrayList<Experience> user_experiences = (ArrayList<Experience>) this.user.getData(User.REL_WORK_EXP);
         for(Experience experience : user_experiences) {
-            HashMap<String, Object> experience_data = (HashMap<String, Object>) experience.serialize();
-            LocalDate start_date = (LocalDate) experience_data.get(experience.START_TIME);
-            LocalDate end_date = (LocalDate) experience_data.get(experience.END_TIME);
-            String title = (String) experience_data.get(experience.EXPERIENCE_TITLE);
-            String description = (String) experience_data.get(experience.EXPERIENCE_DESCRPTION);
+            LocalDate start_date = (LocalDate) experience.getData(experience.START_TIME);
+            LocalDate end_date = (LocalDate) experience.getData(experience.END_TIME);
+            String title = (String) experience.getData(experience.EXPERIENCE_TITLE);
+            String description = (String) experience.getData(experience.EXPERIENCE_DESCRPTION);
             double experience_score = score_calc_time(start_date, end_date);
             score += experience_score;
         }
