be0019f1c1ff6a64f617f95032b9750b8c27acfa
Domenica Vega <domenica.vega@mail.utoronto.ca>
2021-11-14 02:32:49 -0500

    score_calculate() computes score based on user's related work experiences

--- Project/src/UseCase/Score_Calculator/handler_2.java
+++ Project/src/UseCase/Score_Calculator/handler_2.java
@@ -2,11 +2,12 @@ package UseCase.Score_Calculator;
 
 import Entities.Listing.JobListing;
 import Entities.User.User;
-import org.joda.time.Months;
+import Entities.User.Experience;
 
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class handler_2 extends handler_ext_work{
 
@@ -18,13 +19,16 @@ public class handler_2 extends handler_ext_work{
     @Override
     protected void score_calculate() {
         double score = 0.0;
-        LocalDate start_date = (LocalDate) this.user.getData(User.REL_WORK_EXP);
-        LocalDate end_date = (LocalDate) this.user.getData(User.REL_WORK_EXP);
-        String title = (String) this.user.getData(User.REL_WORK_EXP);
-        ArrayList<String> description = (ArrayList<String>) this.user.getData(User.REL_WORK_EXP);
-        double sub_score = score_calc_time(start_date, end_date);
-
-        score = sub_score * description.size();
+        ArrayList<Experience> user_experiences = (ArrayList<Experience>) this.user.getData(User.REL_WORK_EXP);
+        for(Experience experience : user_experiences) {
+            HashMap<String, Object> experience_data = (HashMap<String, Object>) experience.serialize();
+            LocalDate start_date = (LocalDate) experience_data.get(experience.START_TIME);
+            LocalDate end_date = (LocalDate) experience_data.get(experience.END_TIME);
+            String title = (String) experience_data.get(experience.EXPERIENCE_TITLE);
+            String description = (String) experience_data.get(experience.EXPERIENCE_DESCRPTION);
+            double experience_score = score_calc_time(start_date, end_date);
+            score += experience_score;
+        }
         handler_main.score += score;
 
         }
